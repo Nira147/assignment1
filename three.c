bf4e7bc68bea21b14ed8b98bb2d777434bb5f58c
Nira147 <bomzonnira777@gmail.com>
2026-02-19 18:52:42 -0700

    Add files via upload

---
+++ three.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    float l,b,a,p
+    //inout from users
+    printf("enter length and breadth");
+    scanf("%f%f",&l,&b);
+
+    //taking input values
+    //formula of area of rectange
+    
+    a=l*b;
+    //formula of area of rectangle
+    p=2*(l+b);
+    // printing the output
+    printf("Area=%.2%\n",a);
+    printf("perimeter=%.2f",p);
+
+    return 0;
+
+
+
+}
