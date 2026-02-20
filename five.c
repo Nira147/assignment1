c6e4dccd9bd958b273bd55389c7f732b74f36fd2
Nira147 <bomzonnira777@gmail.com>
2026-02-19 19:00:22 -0700

    Add files via upload

---
+++ five.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+
+    char line[200];
+    int i=0;char=0;space=0;
+    fgets(line,size of (line),stdin);
+    printf("Text:%s",line);
+    while (line[i]!='\0')
+    {
+        if(line[i]=='\n')
+      {
+        space ++;
+    }
+    if(line[i]!='\n')
+    {
+        char++;
+
+    }
+    i++;
+}
+printf("total charaecters=%d\n,chars");
+printf("total spaces=%d\n",spaces);
+
+return 0;
+
+}
+
+
+        
+
+    
+
+
+
+
