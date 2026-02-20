bae50e6ce5b182c692ac80c29cb0db405afe351a
Nira147 <bomzonnira777@gmail.com>
2026-02-19 19:02:50 -0700

    Add files via upload

---
+++ eight.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    float cost,salvage,life,depreciaton;
+    printf("enter coat price");
+    scanf("%f",&cost);
+    printf("enter salvage value and useful life(year)\n:");
+    scanf("%f%f",&salvage,&life);
+    depreciaton=(cost-salvage)|life;
+    printf("%f is depreciation",depreciation);
+     
+    return 0;
+
+}
+
+
+
